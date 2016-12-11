f19150a5bc554a8a2e3484c26011f0bfa36a072f
Michael Nix <mchl.nix@googlemail.com>
2016-12-11 19:12:30 +0100

    adds function to upload tasks to habitrpg and support checklists

--- itemmanager.py
+++ itemmanager.py
@@ -1,8 +1,20 @@
 import item
+import helpers
+
+import todoist
 
 class ItemManager:
     def __init__( self, h_api_user, h_api_key, t_api ):
-        pass
+        self.item_list = {}
+
+        self.api = todoist.TodoistAPI( t_api )
+
+        self.api.sync()
+
+        self.habit = helpers.HabiticaHelper( h_api_user, h_api_key )
+
+        for _item in self.api.items.all():
+            self.item_list[_item["id"]] = item.Item( self.api, self.habit, _item["id"] )
 
     def _load_config( self ):
         pass
@@ -11,5 +23,26 @@ class ItemManager:
         pass
 
     def upload_to_habit( self ):
-        pass
+
+        # upload todos and checklists first
+
+        for _item in self.item_list.values():
+            if _item.indent > 1:
+                continue
+
+            _item.sync_to_habitrpg()
+
+        for _item in self.item_list.values():
+            if _item.indent == 1:
+                continue
+
+            self.habit.upload_checklist_item( {"text": _item.content }, self.item_list[_item.parent].habit_id )
+
+    def delete_all( self ):
+        for _item in self.item_list.values():
+            if _item.is_checklist_item:
+                continue
+
+            _item.delete_from_habitrpg()
+
 
