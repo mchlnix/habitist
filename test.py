17cf6b2beca893d57b5b89a5ab2259c9dd4eeaaf
Michael Nix <mchl.nix@googlemail.com>
2016-12-11 19:13:02 +0100

    now uses the itemmanager class

--- test.py
+++ test.py
@@ -6,27 +6,19 @@ import helpers
 import item
 import time
 
+import itemmanager
+
 # All test accounts
 
 todoist_api_key = "842a5fd93a077424ae29e681fb90551653bd8129"
 habit_api_user  = "4cd37e0d-e73b-4bd9-b203-38b0eeace872"
 habit_api_key   = "cddfbde9-5e50-407f-baa9-8c2ccb8e7506"
 
-api = todoist.TodoistAPI( todoist_api_key )
-
-api.sync()
-
-habit = helpers.HabiticaHelper( habit_api_user, habit_api_key )
-
-items = []
-
-for _item in api.items.all():
-    items += [ item.Item( api, habit, _item["id"] ) ]
+manager = itemmanager.ItemManager( habit_api_user, habit_api_key, todoist_api_key )
 
 print( "Syncing with habitrpg", end="" )
 
-for _item in items:
-    _item.sync_to_habitrpg()
+manager.upload_to_habit()
 
 print("\rSyncing complete      " )
 
@@ -36,8 +28,7 @@ print( "" )
 
 print( "Deleting items.", end="" )
 
-for _item in items:
-    _item.delete_from_habitrpg()
+manager.delete_all()
 
 print( "\rDeleted items  " )
 
