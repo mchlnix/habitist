017634e2086b82bf16c22ea02e3beb32d9831c14
Michael Nix <mchl.nix@googlemail.com>
2016-12-11 15:48:23 +0100

    adds structure for itemmanager

---
+++ itemmanager.py
@@ -0,0 +1,15 @@
+import item
+
+class ItemManager:
+    def __init__( self, h_api_user, h_api_key, t_api ):
+        pass
+
+    def _load_config( self ):
+        pass
+
+    def _save_config( self ):
+        pass
+
+    def upload_to_habit( self ):
+        pass
+
