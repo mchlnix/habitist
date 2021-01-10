8ac6eecce8eef3c5fc185f54efda3fa0245ff6c8
Michael Nix <mchl.nix@googlemail.com>
2021-01-10 20:51:12 +0100

    update rest of code to python3

--- helpers.py
+++ helpers.py
@@ -1,59 +1,61 @@
 import requests as r
 
+
 class HabiticaHelper:
 
-    def __init__( self, user_id, api_key ):
-        self.headers={"x-api-user": user_id,
-                      "x-api-key":  api_key }
-        self.tag_cache={}
+    def __init__(self, user_id, api_key):
+        self.headers = {"x-api-user": user_id, "x-api-key": api_key}
+        self.tag_cache = {}
 
-    '''
-    Gets the ID for a label in habitrpg.
-    
-    label_name      the name to search for
-    create          whether or not to create a label,
-                    if it can't be found
-    use_cache       if false a new request will be made
+    def get_tag_id_by_name(self, tag_name, use_cache=True):
+        """
+        Gets the ID for a label in habitrpg.
 
-    Returns the tag id.
-    '''
-    def get_tag_id_by_name( self, tag_name, create=True, use_cache=True ):
+        label_name      the name to search for
+        create          whether or not to create a label,
+                        if it can't be found
+        use_cache       if false a new request will be made
+
+        Returns the tag id.
+        """
         if not self.tag_cache or not use_cache:
-            resp = r.get( "https://habitica.com/api/v3/tags", headers=self.headers )
+            resp = r.get("https://habitica.com/api/v3/tags", headers=self.headers)
             self.tag_cache = resp.json()["data"]
 
         for tag in self.tag_cache:
             if tag["name"] == tag_name:
                 return tag["id"]
         else:
-            resp = r.post( "https://habitica.com/api/v3/tags", headers=self.headers, json={"name": tag_name} )
-            return self.get_tag_id_by_name( tag_name, use_cache=False )
-        
-    '''
-    Creates a task.
-    
-    task        a dictionary with:  text        -   The name of the task
-                                    date        -   When is it due, if ever.
-                                    tags        -   A list of ids (of habitrpg)
-                                    completed   -   boolean
-                                    type        -   "todo" or "daily"
-                                    dateCreated -   Like 2020-12-06T22:59:59.000Z
-    
-    Returns the task id.
-    '''
-    def upload_task( self, task ):
+            r.post("https://habitica.com/api/v3/tags", headers=self.headers, json={"name": tag_name})
+            return self.get_tag_id_by_name(tag_name, use_cache=False)
+
+    def upload_task(self, task):
+        """
+        Creates a task.
+
+        task        a dictionary with:  text        -   The name of the task
+                                        date        -   When is it due, if ever.
+                                        tags        -   A list of ids (of habitrpg)
+                                        completed   -   boolean
+                                        type        -   "todo" or "daily"
+                                        dateCreated -   Like 2020-12-06T22:59:59.000Z
+
+        Returns the task id.
+        """
         print(task)
         print(self.headers)
-        resp = r.post( "https://habitica.com/api/v3/tasks/user", headers=self.headers, json=task )
-        print(resp.__dict__) 
+        resp = r.post("https://habitica.com/api/v3/tasks/user", headers=self.headers, json=task)
+        print(resp.__dict__)
         return resp.json()["data"]["id"]
 
-    '''
-    Transforms a todoist date string to a habitrpg date string.
+    @staticmethod
+    def date_t_to_h(date_string):
+        """
+        Transforms a todoist date string to a habitrpg date string.
 
-    Returns string.
-    '''
-    def date_t_to_h( self, date_string ):
+        Returns string.
+        """
+
         if not date_string:
             return ""
 
@@ -62,55 +64,54 @@ class HabiticaHelper:
                    "Sep": "09", "Oct": "10", "Nov": "11", "Dec": "12"}
         _l = date_string['date'].split("-")
         time = _l[2].split('T')
-        year  = _l[0]
-        day   = time[0]
+        year = _l[0]
+        day = time[0]
         month = _l[1]
-        if len(time)>1:
-            time  = time[1]
+        if len(time) > 1:
+            time = time[1]
         else:
             time = "23:59:59"
 
-        return "{}-{}-{}T{}.000Z".format( year, month, day, time)
+        return "{}-{}-{}T{}.000Z".format(year, month, day, time)
 
-    '''
-    Uploads a task object to habitrpg to update the task with id habit_id.
+    def update_task(self, habit_id, task):
+        """
+        Uploads a task object to habitrpg to update the task with id habit_id.
 
-    Returns response as json.
-    '''
-    def update_task( self, habit_id, task ):
-        resp = r.put( "https://habitica.com/api/v3/tasks/" + str(habit_id), headers=self.headers, json=task )
-
-        return resp.json()["data"]
-
-    def download_task( self, habit_id ):
-        resp = r.get( "https://habitica.com/api/v3/tasks/" + str(habit_id), headers=self.headers )
+        Returns response as json.
+        """
+        resp = r.put(f"https://habitica.com/api/v3/tasks/{habit_id}", headers=self.headers, json=task)
 
         return resp.json()["data"]
 
-    def delete_task( self, habit_id ):
-        resp = r.delete( "https://habitica.com/api/v3/tasks/" + str(habit_id), headers=self.headers )
+    def download_task(self, habit_id):
+        resp = r.get(f"https://habitica.com/api/v3/tasks/{habit_id}", headers=self.headers)
 
-        return 
+        return resp.json()["data"]
 
-    def upload_checklist_item( self, task, checklist_id ):
-        resp = r.post("https://habitica.com/api/v3/tasks/%s/checklist" % checklist_id, headers=self.headers, json=task )
+    def delete_task(self, habit_id):
+        r.delete(f"https://habitica.com/api/v3/tasks/{habit_id}", headers=self.headers)
+
+        return
+
+    def upload_checklist_item(self, task, checklist_id):
+        resp = r.post(f"https://habitica.com/api/v3/tasks/{checklist_id}/checklist", headers=self.headers, json=task)
 
         try:
-           return resp.json()["data"]["checklist"][-1]["id"]
+            return resp.json()["data"]["checklist"][-1]["id"]
         except KeyError:
             print(resp.json())
 
         return "0"
 
-    def score_task( self, habit_id, direction="up" ):
-        resp = r.post("https://habitica.com/api/v3/tasks/%s/score/%s" % (habit_id, direction), headers=self.headers )
+    def score_task(self, habit_id, direction="up"):
+        r.post(f"https://habitica.com/api/v3/tasks/{habit_id}/score/{direction}", headers=self.headers)
 
-
-    def checklist_item_is_uploaded( self, item_id, parent_id ):
+    def checklist_item_is_uploaded(self, item_id, parent_id):
         if parent_id == "0" or item_id == "0":
             return False
 
-        task = self.download_task( parent_id )
+        task = self.download_task(parent_id)
 
         try:
             if task:
@@ -122,25 +123,24 @@ class HabiticaHelper:
 
         return False
 
-
-
     '''
     Checks if a task is uploaded as a task. Note: check does not work for 
     checklist items. Use checklist_item_is_uploaded, instead.
 
     Returns boolean. True if it is uploaded.
     '''
-    def task_is_uploaded( self, habit_id ):
-        return bool( self.download_task( habit_id ) )
 
-    def score_checklist_item( self, item_id, parent_id ):
-        resp = r.post( "https://habitica.com/api/v3/tasks/%s/checklist/%s/score" % (parent_id, item_id), headers=self.headers )
+    def task_is_uploaded(self, habit_id):
+        return bool(self.download_task(habit_id))
 
-    def checklist_item_is_completed( self, item_id, parent_id ):
+    def score_checklist_item(self, item_id, parent_id):
+        r.post(f"https://habitica.com/api/v3/tasks/{parent_id}/checklist/{item_id}/score", headers=self.headers)
+
+    def checklist_item_is_completed(self, item_id, parent_id):
         if parent_id == "0" or item_id == "0":
             return False
 
-        task = self.download_task( parent_id )
+        task = self.download_task(parent_id)
 
         try:
             if task:
@@ -149,6 +149,5 @@ class HabiticaHelper:
                         return child["completed"]
         except KeyError:
             print(task)
-
 
         return False
